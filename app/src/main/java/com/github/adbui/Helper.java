4d996a9c106fe95e5caae42ba35b75c0e4dc24fd
tangonian <tangonian@tencent.com>
2021-06-19 13:24:18 +0800

    命令行解析

--- app/src/main/java/com/github/adbui/Helper.java
+++ app/src/main/java/com/github/adbui/Helper.java
@@ -1,11 +1,13 @@
 package com.github.adbui;
 
+import android.graphics.Path;
 import android.util.Log;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
@@ -19,15 +21,17 @@ import static com.github.adbui.MainActivity.getString;
 
 public class Helper {
     private static final String TAG = "tango";
-    private static final String PROCESS_NAME = "helloworld.cli";
+    private static final String PROCESS_NAME = "adbui helper";
     private static final String VERSION = "1.0";
 
     public static void main(String[] args) {
-        setArgV0(PROCESS_NAME);
+//        setArgV0(PROCESS_NAME);
 
         Options options = new Options();
         options.addOption("v", "version", false, "show current version");
         options.addOption("h", "help", false, "show this message");
+        options.addOption(Option.builder("m").longOpt("md5").argName("path").desc("获取指定文件的 md5 值").build());
+        options.addOption(Option.builder("d").longOpt("download").argName("url> <path").numberOfArgs(2).valueSeparator(' ').desc("下载文件").build());
 
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
@@ -36,27 +40,20 @@ public class Helper {
         try {
             cmd = parser.parse(options, args);
         } catch (ParseException e) {
-            System.err.println(e.getMessage());
             formatter.printHelp(PROCESS_NAME, options);
+            System.err.println(e.getMessage());
             System.exit(1);
             return;
         }
 
         if (cmd.hasOption("help")) {
             formatter.printHelp(PROCESS_NAME, options);
-            return;
-        }
-        if (cmd.hasOption("version")) {
+        } else if (cmd.hasOption("version")) {
             System.out.println(VERSION);
-            return;
+        } else if (cmd.hasOption("md5")) {
+            String path = cmd.getOptionValue("m");
+            System.out.println(path);
         }
-
-
-
-
-
-
-
 
 
 
